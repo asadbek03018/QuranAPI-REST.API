5ff41d1357d33db3d72ddf7893c09a34721853c4
Asadbek <abdupattoyevsardorbekjon@gmail.com>
2024-08-09 22:44:41 +0500

    Update: V3

--- config/settings.py
+++ config/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -136,7 +137,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR/'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media/'
